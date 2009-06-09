f1a61bf17cf3f4526ec5d593ae0523d7f544cd9d
mikhailberis <mikhailberis@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2009-06-09 07:05:21 +0000

    Adding convenience header include for bloom_filter.
    git-svn-id: https://svn.boost.org/svn/boost/sandbox/bloom_filter@53768 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ trunk/boost/bloom_filter.hpp
@@ -0,0 +1,12 @@
+#ifndef BOOST_BLOOM_FILTER_20090609_0
+#define BOOST_BLOOM_FILTER_20090609_0
+
+// Copyright 2009 (c) Dean Michael Berris <mikhailberis@gmail.com>
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/bloom_filter/bloom_filter.hpp>
+
+#endif
+
