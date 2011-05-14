0743b127e465059e23ce1d563c6b79f7d74d0485
alejandro <alejandro@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-05-14 18:18:22 +0000

    Prototype implementation of a static bloom filter using std::tuple and recursive template evaluation to manage all hash function classes.
    git-svn-id: https://svn.boost.org/svn/boost/sandbox/bloom_filter@71944 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ branches/prototype/bloom.hpp
@@ -0,0 +1,76 @@
+#ifndef _BLOOM_HPP
+#define _BLOOM_HPP 1
+
+#include <bitset>
+#include <tuple>
+#include "hash.hpp"
+
+template <size_t N,
+	  typename T,
+	  size_t Size,
+	  class HashFunctions>
+struct apply_hash
+{
+  static void insert(const T& t, std::bitset<Size>& _bits) {
+    typedef typename std::tuple_element<N, HashFunctions>::type Hash;
+    _bits[Hash::hash(t) % Size] = true;
+    apply_hash<N-1, T, Size, HashFunctions>::insert(t, _bits);
+  }
+
+  static bool contains(const T& t, const std::bitset<Size>& _bits) {
+    typedef typename std::tuple_element<N, HashFunctions>::type Hash;
+    return (_bits[Hash::hash(t) % Size] && 
+	    apply_hash<N-1, T, Size, HashFunctions>::contains(t, _bits));
+  }
+};
+
+template <typename T,
+	  size_t Size,
+	  class HashFunctions>
+struct apply_hash<0, T, Size, HashFunctions>
+{    
+  static void insert(const T& t, std::bitset<Size>& _bits) {
+    typedef typename std::tuple_element<0, HashFunctions>::type Hash;
+    _bits[Hash::hash(t) % Size] = true;
+  }
+
+  static bool contains(const T& t, const std::bitset<Size>& _bits) {
+    typedef typename std::tuple_element<0, HashFunctions>::type Hash;
+    return (_bits[Hash::hash(t) % Size]);
+  }
+};
+template <typename T, 
+	  size_t Size,
+	  class HashFunctions = std::tuple<MurmurHash3<T, 3>, 
+					   MurmurHash3<T, 5>,
+					   MurmurHash3<T, 7> > >
+class bloom_filter {
+  typedef std::bitset<Size> Bitset;
+
+public:
+  bloom_filter() 
+  {
+  }
+
+  constexpr size_t size() const {
+    return Size;
+  }
+
+  void insert(const T& t) {
+    static const unsigned size = std::tuple_size<HashFunctions>::value - 1;
+    apply_hash<size, T, Size, HashFunctions>::insert(t, bits);
+  }
+
+  bool contains(const T& t) const {
+    static const unsigned size = std::tuple_size<HashFunctions>::value - 1;
+    return apply_hash<size, T, Size, HashFunctions>::contains(t, bits);
+  }
+
+  bool operator[](const T& t) const {
+    return this->contains(t);
+  }
+
+private:
+  std::bitset<Size> bits;
+};
+#endif
---
+++ branches/prototype/hash.hpp
@@ -0,0 +1,98 @@
+#ifndef _HASH_HPP_
+#define _HASH_HPP_ 1
+
+#include <cstdint>
+
+template <typename UnsignedIntT>
+inline UnsignedIntT rotl(const UnsignedIntT x, uint8_t r)
+{
+  return (x << r) | (x >> (sizeof(UnsignedIntT) * 4 - r));
+}
+
+inline uint32_t rotl32(const uint32_t x, uint8_t r)
+{
+  return rotl<uint32_t>(x,r);
+}
+
+inline uint64_t rotl64(const uint64_t x, uint8_t r)
+{
+  return rotl<uint64_t>(x,r);
+}
+
+template <typename UnsignedIntT>
+inline UnsignedIntT get_block(const UnsignedIntT * p, const int i)
+{
+  return p[i];
+}
+
+inline uint32_t fmix(uint32_t h)
+{
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
+void murmurhash3(const void *const key, const int len,
+		 const uint32_t seed, void *out)
+{
+  static const uint32_t c1 = 0xcc9e2d51;
+  static const uint32_t c2 = 0x1b873593;
+
+  const uint8_t *const  data = static_cast<const uint8_t *const>(key);
+  
+  uint32_t h1 = seed;
+  const uint32_t *const blocks = 
+    reinterpret_cast<const uint32_t *const >(data + len);
+
+  for (int i = -(len/4); i; ++i) {
+    uint32_t k1 = blocks[i];
+    
+    k1 *= c1;
+    k1 = rotl32(k1, 15);
+    k1 *= c2;
+
+    h1 ^= k1;
+    h1 = rotl32(h1, 13);
+    h1 = h1*5 + 0xe6546b64;
+  }
+  
+  const uint8_t *const tail = 
+    static_cast<const uint8_t *const>(data + len);
+
+  uint32_t k1 = 0;
+
+  switch (len & 3) {
+  case 3: 
+    k1 ^= tail[2] << 16;
+  case 2: 
+    k1 ^= tail[1] << 8;
+  case 1: 
+    k1 ^= tail[0];
+    k1 *= c1;
+    k1 = rotl32(k1, 16);
+    k1 *= c2;
+    h1 ^= k1;
+  }
+
+  h1 ^= len;
+  h1 = fmix(h1);
+  
+  *static_cast<uint32_t *>(out) = h1;
+}
+
+template <typename T, size_t Seed>
+struct MurmurHash3 {
+  static size_t hash(const T& t) {
+    size_t out = 0;
+    murmurhash3(static_cast<const void *const>(&t), 
+		sizeof(t),
+		Seed,
+		&out);
+    return out;
+  }
+};
+#endif
