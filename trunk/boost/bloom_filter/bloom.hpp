a77cbe29f77bb2a102efe09dad9c691aa7dd49a9
alejandro <alejandro@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2011-06-24 08:27:46 +0000

    Updated reference documentation to match latest additions to implementation.
    git-svn-id: https://svn.boost.org/svn/boost/sandbox/bloom_filter@72731 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- trunk/boost/bloom_filter/bloom.hpp
+++ trunk/boost/bloom_filter/bloom.hpp
@@ -51,7 +51,7 @@ namespace boost {
       }
 
 #ifndef BOOST_NO_0X_HDR_INITIALIZER_LIST
-      bloom_filter(std::initializer_list<T> ilist) {
+      bloom_filter(const std::initializer_list<T>& ilist) {
 	typedef typename std::initializer_list<T>::const_iterator citer;
 	for (citer i = ilist.begin(), end = ilist.end(); i != end; ++i) {
 	  this->insert(*i);
